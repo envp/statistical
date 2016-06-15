597c433cd5ec4d389f41b0e42539822c44637797
Vaibhav Yenamandra <yvvaibhav@gmail.com>
2016-06-15 02:47:25 +0000

    Added tests for Statistical::Domain

--- spec/statistical_spec.rb
+++ spec/statistical_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
+require 'statistical/helpers'
 
 describe Statistical do
-  skip
+  describe Statistical::Domain do
+    exclude = [1, 2, 3, 4]
+    d = Statistical::Domain[0, 5, :closed, *exclude]
+    
+    it {expect(Statistical::Domain.superclass).to eq(Range)}
+    
+    it '::[begin, end, type] creates a new instance' do
+      expect(Statistical::Domain[1, 2, :closed]).to be_a(Statistical::Domain)
+    end
+    
+    it 'adds elements in the exclusion list at initialization' do
+      expect(d.exclusions).to eq(exclude)
+    end
+    
+    it 'does not include elements in the exclusion list' do
+      expect(d.include? exclude[rand(exclude.size)]).to be false
+    end
+  end
 end
